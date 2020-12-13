e0d8aef87d42e3cca2b3cec03efc0cb7b19d3418
Rene Ruck <671493+rruck@users.noreply.github.com>
2020-12-13 16:56:10 +0000

    Day 8 - Part 1

--- src/main/scala/Day8.scala
+++ src/main/scala/Day8.scala
@@ -4,23 +4,46 @@ import scala.language.implicitConversions
 
 object Day8 extends App {
 
-    sealed trait Operation
-    case object NOP           extends Operation
-    case class  ADD(add: Int) extends Operation
-    case class  JMP(jmp: Int) extends Operation
+  sealed trait Operation
 
-    val toOperation: String => Operation = {
-        case s"NOP" => NOP
-        case s"ADD $n" => ADD(n.toInt)
-        case s"JMP $n" => JMP(n.toInt)
-    }
+  case object NOP extends Operation
 
-    val input = Source.fromResource("./input_day_8.txt").getLines
+  case class ACC(add: Int) extends Operation
+
+  case class JMP(jmp: Int) extends Operation
+
+  case class State(pos: Int = 0, acc: Int = 0)
+
+  val toOperation: String => Operation = {
+    case s"nop $_" => NOP
+    case s"acc $n" => ACC(n.toInt)
+    case s"jmp $n" => JMP(n.toInt)
+  }
+
+  val input = Source.fromResource("./input_day_8.txt").getLines.toArray
 
 
+  val operations = input.map(toOperation)
 
-    input.map(toOperation).pipe(println)
 
+  val execute: (State, Operation) => State = (state, op) => op match {
+    case NOP => state.copy(pos = state.pos + 1)
+    case ACC(add) => State(state.pos + 1, state.acc + add)
+    case JMP(jmp) => state.copy(state.pos + jmp)
+  }
 
+  val endExecution: (Int, List[Int]) => Boolean  = (nextPos, history) => history.contains(nextPos)
+
+  def run(state: State, history: List[Int] = List.empty): State = {
+    val op = operations(state.pos)
+    val newState = execute(state, op)
+    if(endExecution(newState.pos, history))
+      newState
+    else
+      run(newState, history :+ newState.pos)
+  }
+
+  val state = run(State())
+  println(state)
 
 }
